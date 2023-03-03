46998c5aae87931c92cfe23fd405d34c0dba732e
SenSei <you@example.com>
2023-03-03 00:02:12 +0000

    Add test for status endpoit

--- ship_game/game_app/tests.py
+++ ship_game/game_app/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
 
-# Create your tests here.
+
+class StatusTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_status(self):
+        response = self.client.get(('/api/status'))
+
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        self.assertEqual(response.data,{'status':'ok'})
